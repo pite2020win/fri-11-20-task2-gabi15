4e02a2f3403cb6a0f3528db6a7f7bb7c27bf659e
repl.it user <replituser@example.com>
2020-10-30 11:51:47 +0000

    brief of task

--- task.py
+++ task.py
@@ -15,7 +15,7 @@
 # If you have even more courage, try implementing user interface (might be text-like).
 #
 #Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
+#Think of as many features as youcan, and try implementing them.
 #Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
 #Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
 #The goal of this task is for you to SHOW YOUR BEST python programming skills.
@@ -28,3 +28,46 @@
 #
 #Delete these comments before commit!
 #Good luck.
+
+def average(lista):
+  sum = 0;
+  for el in lista:
+    sum+=el
+  return sum/len(lista)
+
+
+class Student:
+  def __init__(self, first_name, surname):
+    self.first_name = first_name
+    self.surname = surname
+    self.grades = []
+  def student_average(self):
+    return average(grades)
+
+class SchoolClass:
+  def __init__(self, name):
+    self.name = name
+    self.students = []
+  def append_student(self,s):
+    students.append(s)
+  def class_average(self):
+    stud_averages = []
+    for stud in students:
+      stud_averages.append(average(el))
+    return average(stud_averages)
+
+
+class School:
+  def __init__(self, school_name):
+    self.school_name = school_name;
+    self.classes = []
+  def append_class(c):
+    classes.append(c)
+
+
+if __name__ =='__main__':
+  s1 = Student('Marek', 'Kowalski')
+  s2 = Student ('Jarek', 'Mały')
+#   sclass = SchoolClass('1A')
+#   sclass.append_student(s1)
+#   sclass.append_student(s2)
